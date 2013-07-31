ddf706f714bc7f0d56ef199be82e09f9065f5b6a
OlIvIeR77 <ducrouxolivier@gmail.com>
2013-07-31 19:39:01 +0200

    ii

--- config/recipes/puma.rb
+++ config/recipes/puma.rb
@@ -3,12 +3,17 @@ set_default(:puma_state) { "#{shared_path}/tmp/pids/puma.state" }
 set_default(:puma_sock) { "#{shared_path}/tmp/sockets/puma.sock" }
 set_default(:puma_sock_ctl) { "#{shared_path}/tmp/sockets/pumactl.sock" }
 set_default(:puma_config) { "#{shared_path}/config/puma.rb" }
+set_default(:puma_user) { user }
 
 namespace :puma do
 	desc "Setup puma config"	
 	task :setup, roles: :app do
 		run "mkdir -p #{shared_path}/config"
 		template "puma.rb.erb", puma_config
+		template "puma_init.erb", "/tmp/puma_init"
+	    run "chmod +x /tmp/puma_init"
+	    run "#{sudo} mv /tmp/puma_init /etc/init.d/puma_#{application}"
+	    run "#{sudo} update-rc.d -f puma_#{application} defaults"
 	end
 	after "deploy:setup", "puma:setup"
 end
