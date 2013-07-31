cbadf3f0ba88680d1a9de8ba52ec635dae3999e4
OlIvIeR77 <ducrouxolivier@gmail.com>
2013-07-31 16:54:50 +0200

    ii

---
+++ config/deploy.rb
@@ -0,0 +1,29 @@
+require "bundler/capistrano"
+require "rvm/capistrano"
+
+load "config/recipes/base"
+load "config/recipes/node"
+#load "config/recipes/nginx"
+#load "config/recipes/unicorn"
+load "config/recipes/mysql"
+load "config/recipes/rvm"
+
+server "olivierdo.no-ip.org", :web, :app, :db, primary: true
+
+set :site_url, "olivierdo.no-ip.org"
+set :application, "pumaApp"
+set :user, "olivierdoe"
+
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :use_sudo, false
+
+set :scm, "git"
+set :repository,  "https://github.com/OlIvIeR77/#{application}.git"
+set :branch, "master"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PumaApp::Application.config.secret_token = '5efb94677926bd3b3a3edd728aabba9c96fd4c3bfeeb94b1393b99c2211fee260f0a104018d383500b63a638f98533d018804a199f8cddef33d11a5608a59863'
