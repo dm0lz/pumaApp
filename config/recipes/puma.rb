52525a014c75737ae44abaefc874b3db108fc7ff
OlIvIeR77 <ducrouxolivier@gmail.com>
2013-07-31 21:27:06 +0200

    ii

--- config/recipes/puma.rb
+++ config/recipes/puma.rb
@@ -6,6 +6,7 @@ set_default(:puma_config) { "#{shared_path}/config/puma.rb" }
 set_default(:puma_user) { user }
 
 namespace :puma do
+
 	desc "Setup puma config"	
 	task :setup, roles: :app do
 		run "mkdir -p #{shared_path}/config"
@@ -16,4 +17,13 @@ namespace :puma do
 	    run "#{sudo} update-rc.d -f puma_#{application} defaults"
 	end
 	after "deploy:setup", "puma:setup"
+
+	%w[start stop restart].each do |command|
+    desc "#{command} unicorn"
+    task command, roles: :app do
+      run "service puma_#{application} #{command}"
+    end
+    after "deploy:#{command}", "puma:#{command}"
+  end
+  
 end
